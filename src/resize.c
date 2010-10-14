584c94efebf26a36bec7f5393f234b0bdeb73abe
Kenta Murata <mrkn@mrkn.jp>
2010-10-14 11:12:32 +0900

    apply macports/patch-windowsize

--- src/resize.c
+++ src/resize.c
@@ -664,6 +664,17 @@ int wi, he, hi;
       he = 1000;
     }
 
+  if (wi > 1000)
+    {
+      Msg(0, "Window width too large, truncated");
+      wi = 1000;
+    }
+  if (he > 1000)
+    {
+      Msg(0, "Window height too large, truncated");
+      he = 1000;
+    }
+
   if (p->w_width == wi && p->w_height == he && p->w_histheight == hi)
     {
       debug("ChangeWindowSize: No change.\n");
